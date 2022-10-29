ce269d61336719e4b91b2e9cb58c68187d0e494a
egelirli <egelirli@yahoo.com>
2022-10-29 23:10:22 +0300

    use feign client

--- currency-conversion-service/src/main/java/com/egelirli/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/egelirli/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.egelirli.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +12,14 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
-
+	
+	 //@Autowired
+	 private CurrencyExchangeProxy  excahngeProxy;
+	
+	public  CurrencyConversionController(CurrencyExchangeProxy excahngeProxy) {
+		this.excahngeProxy = excahngeProxy;
+	}
+	 
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
 			@PathVariable String from,
@@ -40,5 +48,22 @@ public class CurrencyConversionController {
 	
 	}
 	
-	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+			) {
+		
+	     CurrencyConversion currencyConversion = excahngeProxy.retrieveExchangeValue(from, to);
+		
+		return new CurrencyConversion(currencyConversion.getId(), 
+				from, to, quantity, 
+				currencyConversion.getExchangeRate(), 
+				quantity.multiply(currencyConversion.getExchangeRate()), 
+				currencyConversion.getEnvironment() + " " + "feign");
+		
+	}
+				
+					
 }
