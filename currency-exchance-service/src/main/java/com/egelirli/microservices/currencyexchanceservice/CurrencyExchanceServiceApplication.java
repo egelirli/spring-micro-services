5a4233cba61d9d437bc053672e80042ba1e68712
egelirli <egelirli@yahoo.com>
2022-10-30 00:21:31 +0300

    NamingServer and load balancing

--- currency-exchance-service/src/main/java/com/egelirli/microservices/currencyexchanceservice/CurrencyExchanceServiceApplication.java
+++ currency-exchance-service/src/main/java/com/egelirli/microservices/currencyexchanceservice/CurrencyExchanceServiceApplication.java
@@ -3,6 +3,7 @@ package com.egelirli.microservices.currencyexchanceservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CurrencyExchanceServiceApplication {
 
