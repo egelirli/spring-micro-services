155266e6a8a03f8fa2474ba5793810eff7220bd9
egelirli <egelirli@yahoo.com>
2022-10-30 21:53:04 +0300

    Circuit breaker started - play with retay and fallback

---
+++ currency-exchance-service/src/main/java/com/egelirli/microservices/currencyexchanceservice/CircuitBreakerController.java
@@ -0,0 +1,37 @@
+package com.egelirli.microservices.currencyexchanceservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+public class CircuitBreakerController {
+	
+	private Logger logger = 
+				LoggerFactory.getLogger(CircuitBreakerController.class);
+	
+	
+	
+	//@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	//@RateLimiter(name="default")
+	//@Bulkhead(name="sample-api")
+	@GetMapping("/sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	public String sampleApi() {
+		logger.info("Sample api call received");
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", 
+					String.class);
+		return forEntity.getBody();
+//		return "sample-api";
+	}
+	
+	public String hardcodedResponse(Exception ex) {
+		return "fallback-response";
+	}
+}
